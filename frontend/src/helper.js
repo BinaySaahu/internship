b0b88205d0bc609f3aa115926a89e165f86cded3
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-06-01 10:25:55 +0530

    commit

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://backend-3lno.onrender.com"
-// export const BASE_URL = "http://localhost:4000"
+// export const BASE_URL = "https://backend-3lno.onrender.com"
+export const BASE_URL = "http://localhost:4000"
