feead4030fd1085d8ec41ead3190b9f4a090b668
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-06-01 10:27:20 +0530

    commit

--- backend/controllers/sendEmail.js
+++ backend/controllers/sendEmail.js
@@ -1,6 +1,7 @@
 const mailer = require("../mailer/mailer");
 
 const sendEmail = async(req,res)=>{
+    let array = req.body;
     var mail = {
         to: "21052245@kiit.ac.in",
         subject: "Data of Selected rows",
@@ -9,9 +10,59 @@ const sendEmail = async(req,res)=>{
         <head>
             <meta charset="UTF-8">
             <title>Email Verification</title>
+            <style>
+            table {
+                border-collapse: collapse;
+                width: 99%;
+                color: #333;
+                font-family: Arial, sans-serif;
+                font-size: 14px;
+                text-align: left;
+                border-radius: 10px;
+                overflow: hidden;
+                box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+                margin: auto;
+                margin-top: 50px;
+                margin-bottom: 50px;
+              }
+              table th {
+                background-color: #4CAF50;
+                color: #fff;
+                font-weight: bold;
+                padding: 10px;
+                text-transform: uppercase;
+                letter-spacing: 1px;
+                border-top: 1px solid #4CAF50;
+                border-bottom: 1px solid #4CAF50;
+                height: 20px;
+              }
+              
+              table td {
+              
+                padding: 10px;
+                border-bottom: 1px solid rgb(49 55 71);
+                font-weight: bold;
+              }
+              
+
+            </style>
         </head>
         <body>
-            <div>${JSON.stringify(req.body)}</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Phone Number</th>
+                        <th>Hobbies</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr><TD>${array.map(e=>Object.values(e)
+
+                    .join('<TD>')).join('<tr><TD>')}
+                </tbody>
+            </table>
         </body>
         </html>`,
       };
